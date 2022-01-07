c21b3f0076f7f80e984bde72413170005cbf0208
yashpatel5400 <ypatel5400@gmail.com>
2022-01-07 15:28:29 -0500

    Not sure anymore

--- main.py
+++ main.py
@@ -258,4 +258,5 @@ def home_page():
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host = '0.0.0.0')
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host = '0.0.0.0', port=port)
