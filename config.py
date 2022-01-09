d5bce839f9aa6884f8f8e0bebd0b240cbbabe961
yashpatel5400 <ypatel5400@gmail.com>
2022-01-09 17:29:45 -0500

    Fixing leaderboard

--- config.py
+++ config.py
@@ -4,6 +4,6 @@ print(basedir)
 
 class Config(object):
     # ...
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://", 1) or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
