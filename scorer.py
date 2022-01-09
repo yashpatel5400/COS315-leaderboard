ed762fa4a87348c12b31fa227d3b580e0b817d97
yashpatel5400 <ypatel5400@gmail.com>
2022-01-09 15:30:02 -0500

    Finishing touches

--- scorer.py
+++ scorer.py
@@ -1,5 +1,7 @@
 import pandas as pd 
 import pickle
+import urllib.request
+import os
 
 from sklearn.metrics import mean_squared_error
 
@@ -17,8 +19,22 @@ class Scorer():
         with open(submission_path, 'rb') as f:
             model = pickle.load(f)
 
-        print(model)
-        score = 1.0
+        # download raw dataset CSV
+        test_fn = "air_quality_test.csv"
+
+        if not os.path.exists(test_fn):
+            dataset_url = "https://yashpatel5400.github.io/files/datasets/air_quality_xyz_test.csv"
+            urllib.request.urlretrieve(dataset_url, test_fn)
+
+        # load dataset into numpy
+        df = pd.read_csv("air_quality.csv", sep=";",header=0, decimal=',')
+        df = df[df["Date"] == df["Date"]]
+
+        X = df.values[:,3:-2]
+        y = df.values[:,2]
+        y_hat = model.predict(X)
+
+        score = sklearn.metrics.r2_score(y, y_hat)
 
         # if y_submission.isna().sum() > 0:
         #     return ("SUBMISSION HAS NULL VALUE", None)
